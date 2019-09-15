f29aac6458676a6657ec1822b020ba00009b74b9
Bruno Manarin <brunomanarin@icloud.com>
2019-09-14 21:36:42 -0300

    Adicione implementação éosguri

--- ListaDuplaEncadeada/src/Main.java
+++ ListaDuplaEncadeada/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+    	ListaDuplamenteEncadeada teste = new ListaDuplamenteEncadeada();
+    	
+    	teste.insereNoComeco("Bruno", 20, "Sistemas de éosguri");
     }
 }
